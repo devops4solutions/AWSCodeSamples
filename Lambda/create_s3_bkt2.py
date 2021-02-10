8d9e94adc6f2cce2a57c2cfcd162d2e4a26fd043
Nidhi Gupta <devops4solutions@gmail.com>
2021-02-10 13:07:05 -0500

    initial commit

---
+++ Lambda/create_s3_bkt2.py
@@ -0,0 +1,10 @@
+import boto3
+
+bucketName = "devops4solutions-target"
+bucketRegion = "us-east-2"
+
+# Creates an s3 Resource; this is a higher level API type service for s3.
+s3 = boto3.resource('s3')
+
+# Creates a bucket
+bucket = s3.create_bucket(ACL='private',Bucket=bucketName,CreateBucketConfiguration={'LocationConstraint': bucketRegion})
